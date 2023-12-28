a2ae4012b7f012bfb787e881cb3a7d75fba0fd14
proskovey <93362757+proskovey@users.noreply.github.com>
2023-12-28 17:02:00 +0500

    Update test.js

--- src/__tests__/test.js
+++ src/__tests__/test.js
@@ -1,5 +1,18 @@
-import GameSavingLoader from '../app';
+import GameSavingLoader from '../GameSavingLoader';
 
-test('Game Saving', () => GameSavingLoader.load().then((data) => {
-  expect(data).toBe('{"id":9,"created":1546300800,"userInfo":{"id":1,name":"Hitman","level":10,"points":2000}}');
-}));
+test('Проверка работы  GameSavingLoader', (done) => {
+  const expected = JSON.stringify({
+    id: 9,
+    created: 1546300800,
+    userInfo: {
+      id: 1,
+      name: 'Hitman',
+      level: 10,
+      points: 2000,
+    },
+  });
+  GameSavingLoader.load().then((saving) => {
+    expect(saving).toEqual(expected);
+    done();
+  });
+});
